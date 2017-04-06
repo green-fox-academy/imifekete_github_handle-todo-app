8cc6c34717836c38a050b0c469929db7c12da0bb
imifekete <imifekete@gmail.com>
2017-04-06 15:09:03 +0200

    tasks

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Fekete Imre on 2017.04.06..
+ */
+public class Main {
+  public static void main(String[] args) {
+
+  }
+}
---
+++ src/Tasks.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Fekete Imre on 2017.04.06..
+ */
+public class Tasks {
+}
