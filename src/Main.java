add58709023b3fc1eddfa815ae10fdb2aac0eb03
imifekete <imifekete@gmail.com>
2017-04-06 22:14:22 +0200

    first few feature

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,27 @@
-/**
- * Created by Fekete Imre on 2017.04.06..
- */
 public class Main {
+
   public static void main(String[] args) {
+    Tasks tasks = new Tasks();
+    String tasksFileName = "tasks.csv";
 
-
-    if (args.length ==0) {
+    if (args.length == 0) {
       System.out.println("Java  ToDo application\n" +
               "======================\n" +
               "\n" +
               "Command line arguments:\n" +
               "-l   Lists all the tasks\n" +
               "-a   Adds a new task\n" +
-              "-r   Removes an task\n" +
-              "-c   Completes an task\n");
+              "-r   Removes a task\n" +
+              "-c   Completes a task\n");
+    } else if (args[0].equals("-l")) {
+      tasks.list();
+    }
+    else if (args[0].equals("-a")){
+      if(args[1].length() == 0){
+        //egyik feladat az lesz hogy ide ki kell irni a hibat
+      } else {
+        tasks.add(args[1]);
+      }
     }
   }
 }
--- src/Tasks.java
+++ src/Tasks.java
@@ -1,6 +1,28 @@
-/**
- * Created by Fekete Imre on 2017.04.06..
- */
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Tasks {
 
+  public void list() {
+    try {
+      String content = new String(Files.readAllBytes(Paths.get("tasks")));
+      if ("".equals(content)) {
+        System.out.println("No todos for today! :)");
+      } else {
+        String[] lines = content.split("\n");
+        for (int i = 0; i < lines.length; i++) {
+          System.out.println(i + 1 + " - " + lines[i]);
+        }
+      }
+    } catch (IOException e) {
+      System.out.println(e + "Hiba tortent file beolvasasa kozben");
+    }
+  }
+
+  public void add(String input) {
+    //Google: add a new line to a file
+    System.out.println("hhhhhhhhhhhh");
+  }
 }
