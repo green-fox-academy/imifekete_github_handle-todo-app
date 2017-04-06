c2809e1af693845e25a2f7c107afd38a2d2aa5de
imifekete <imifekete@gmail.com>
2017-04-06 15:34:46 +0200

    Print usage

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,16 @@
 public class Main {
   public static void main(String[] args) {
 
+
+    if (args.length ==0) {
+      System.out.println("Java  ToDo application\n" +
+              "======================\n" +
+              "\n" +
+              "Command line arguments:\n" +
+              "-l   Lists all the tasks\n" +
+              "-a   Adds a new task\n" +
+              "-r   Removes an task\n" +
+              "-c   Completes an task\n");
+    }
   }
 }
---
+++ src/Tasks.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Fekete Imre on 2017.04.06..
+ */
+public class Tasks {
+
+}
